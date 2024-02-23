9d9d80da458b72e0bf4e37e40107a73a6e9a062e
Qendrim Behrami <behrami@in.tum.de>
2024-02-23 20:36:52 +0100

    Setup font and footer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./style.css";
 
-console.log("Hello Webpack!");
+// main.js or another relevant JS file
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faGithub);
+dom.watch();
